d377b94301350f0d520a116a655a2aca589460d4
jinnyfruit <jinnyfruit0527@gmail.com>
2021-01-12 15:58:30 +0900

    primary_type in Java

--- personal_study/Hello_JAVA.java
+++ personal_study/Hello_JAVA.java
@@ -3,7 +3,7 @@ file name: Hello java
 author:jinnyfruit
 modified: 01.12,2021
 */ 
-public class first_java` {
+public class first_java {
     public static  void main(String[] args`){
         System.out.println("hello Java");
     }
---
+++ personal_study/primary_type_in_Java.java
@@ -0,0 +1,18 @@
+/*
+file name: primary_type in Java
+author:jinnyfruit
+modified: 01.12,2021
+*/
+public class primary_type {
+    public static void main(String[] args){
+        boolean smart=true;
+        int age=21;
+        char blood='O';
+        double py=3.141592;
+        String name="Kim Ji Woo";
+        System.out.println("name: "+name);
+        System.out.println("age: "+age);
+        System.out.println("blood_type: "+blood);
+        System.out.println("favorite_number: "+py);
+    }
+}
