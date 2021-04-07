72633a42311d49b67a82e781bbf558ec18d7c426
jinnyfruit <jinnyfruit0527@gmail.com>
2021-04-07 23:59:47 +0900

    2

---
+++ Object_oriented_programming/LAB/String_methods.java
@@ -0,0 +1,16 @@
+ /*
+ file name: string methods test
+ author: Ji Woo Kim
+ modified; 04.07, 2021
+*/
+import java.util.Scanner;
+ public class String_methods {
+    public static void main (String[] args){
+
+        String greeting = "Hello";
+
+        int num = greeting.length();
+        System.out.println("length of Hello is " + num);
+
+    }
+}
