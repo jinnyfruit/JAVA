9c3e40425514c0404f2a7d7f7836b981e3a12d33
jinnyfruit <jinnyfruit0527@gmail.com>
2021-05-13 01:26:37 +0900

    6-1 test

---
+++ Object_oriented_programming/LAB/PetTest.java
@@ -0,0 +1,12 @@
+/*
+ file name: 6-1
+ author: Ji woo Kim
+ modified; 05.13 , 2021
+*/
+public class PetTest {
+
+    public static void main(String[] args) {
+        Pet dog = new Pet("Cloud", 3, 5);
+        dog.writeOutput();
+    }
+}
